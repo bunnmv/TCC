3dd8308da1b5ee6089f33c71218c05da85c2e26d
bunnmv <bunnmv@gmail.com>
2018-04-23 14:09:27 -0300

    Tests on Sync Block and CRC32

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Apr 12 12:54:49 2018
+# Generated: Mon Apr 23 14:08:00 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -22,12 +22,14 @@ from gnuradio import channels
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import fec
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
+import math
 import numpy
 import pmt
 import sip
@@ -72,15 +74,16 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         self.packetCounterLength = packetCounterLength = 1
         self.infoLength = infoLength = 22*10
-        self.payloadLength = payloadLength = (infoLength+packetCounterLength)
-        self.sps = sps = 4
-        self.nfilts = nfilts = 32
-        self.frameLength = frameLength = payloadLength+12
+        self.packetLength = packetLength = (infoLength+packetCounterLength+4)
+        self.sps = sps = 8
+        self.nfilts = nfilts = 64
+        self.frameLength = frameLength = packetLength+12
         self.excess_bw = excess_bw = 0.35
         self.snr = snr = 0
         self.samp_rate = samp_rate = 32000
         self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts*sps, 1.0, excess_bw, 45*nfilts)
         self.order_costas = order_costas = 8
+        self.dataLength = dataLength = (infoLength+packetCounterLength)
 
         self.MO = MO = digital.constellation_8psk().base()
 
@@ -112,7 +115,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
 
         self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_TAG, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "len_key2")
         self.qtgui_time_sink_x_0_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0_0.enable_grid(False)
         self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
@@ -189,10 +192,12 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_1_win = sip.wrapinstance(self.qtgui_const_sink_x_1.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_1_win)
-        self.fec_extended_tagged_encoder_0 = fec.extended_tagged_encoder(encoder_obj_list=CE, puncpat='11', lentagname='len_key', mtu=payloadLength)
-        self.fec_extended_tagged_decoder_0 = self.fec_extended_tagged_decoder_0 = fec_extended_tagged_decoder_0 = fec.extended_tagged_decoder(decoder_obj_list=CD, ann=None, puncpat='11', integration_period=10000, lentagname='len_key2', mtu=payloadLength)
+        self.fec_extended_tagged_encoder_0 = fec.extended_tagged_encoder(encoder_obj_list=CE, puncpat='11', lentagname='len_key', mtu=packetLength)
+        self.fec_extended_tagged_decoder_0 = self.fec_extended_tagged_decoder_0 = fec_extended_tagged_decoder_0 = fec.extended_tagged_decoder(decoder_obj_list=CD, ann=None, puncpat='11', integration_period=10000, lentagname='len_key2', mtu=packetLength)
+        self.digital_symbol_sync_xx_0_0 = digital.symbol_sync_cc(digital.TED_SIGNAL_TIMES_SLOPE_ML, sps, 6.82/100, 2*nfilts, 1.0, 1.5, 1, digital.constellation_8psk().base(), digital.IR_PFB_MF, nfilts, (rrc_taps))
         self.digital_protocol_formatter_bb_0 = digital.protocol_formatter_bb(hdr_format, 'len_key')
-        self.digital_pfb_clock_sync_xxx_0_0 = digital.pfb_clock_sync_ccf(sps, 6.82/100, (rrc_taps), nfilts, nfilts/2, 1.5, 1)
+        self.digital_crc32_bb_0_0 = digital.crc32_bb(True, "len_key2", True)
+        self.digital_crc32_bb_0 = digital.crc32_bb(False, "len_key", True)
         self.digital_costas_loop_cc_0 = digital.costas_loop_cc(6.82/100, order_costas, False)
         self.digital_correlate_access_code_xx_ts_0 = digital.correlate_access_code_ff_ts(digital.packet_utils.default_access_code,
           0, 'len_key2')
@@ -207,29 +212,29 @@ class top_block(gr.top_block, Qt.QWidget):
           log=False,
           )
         self.channels_channel_model_0 = channels.channel_model(
-        	noise_voltage=snr,
+        	noise_voltage=0,
         	frequency_offset=1e-4,
         	epsilon=1.001,
         	taps=(1.0 + 1.0j, ),
         	noise_seed=0,
-        	block_tags=True
+        	block_tags=False
         )
         self.blocks_vector_source_x_0 = blocks.vector_source_b(numpy.arange(0,256), True, 1, [])
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
         self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'len_key', 0)
-        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, payloadLength, "len_key")
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, dataLength, "len_key")
         self.blocks_stream_mux_0 = blocks.stream_mux(gr.sizeof_char*1, (packetCounterLength, infoLength))
         self.blocks_repack_bits_bb_0_0_0_0_0_0 = blocks.repack_bits_bb(1, 8, 'len_key', False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_0_0_0_0 = blocks.repack_bits_bb(8, 1, 'len_key', False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_0_0_0 = blocks.repack_bits_bb(1, 8, 'len_key2', False, gr.GR_MSB_FIRST)
-        self.blocks_keep_m_in_n_0_0 = blocks.keep_m_in_n(gr.sizeof_char, infoLength, payloadLength, packetCounterLength)
-        self.blocks_keep_m_in_n_0 = blocks.keep_m_in_n(gr.sizeof_char, packetCounterLength, payloadLength, 0)
+        self.blocks_keep_m_in_n_0_0 = blocks.keep_m_in_n(gr.sizeof_char, infoLength, dataLength, packetCounterLength)
+        self.blocks_keep_m_in_n_0 = blocks.keep_m_in_n(gr.sizeof_char, packetCounterLength, dataLength, 0)
         self.blocks_head_0 = blocks.head(gr.sizeof_char*1, 110000)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/sender.txt', True)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/test2.txt', False)
+        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/header_counter.txt', False)
         self.blocks_file_sink_0_0_0.set_unbuffered(True)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/test.txt', False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/payload.txt', False)
         self.blocks_file_sink_0_0.set_unbuffered(True)
         self.blocks_char_to_float_1_1_0 = blocks.char_to_float(1, 1)
 
@@ -243,24 +248,26 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_head_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.blocks_keep_m_in_n_0, 0), (self.blocks_file_sink_0_0_0, 0))
         self.connect((self.blocks_keep_m_in_n_0_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.blocks_keep_m_in_n_0, 0))
-        self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.blocks_keep_m_in_n_0_0, 0))
+        self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.digital_crc32_bb_0_0, 0))
         self.connect((self.blocks_repack_bits_bb_0_0_0_0, 0), (self.fec_extended_tagged_encoder_0, 0))
         self.connect((self.blocks_repack_bits_bb_0_0_0_0_0_0, 0), (self.blocks_tagged_stream_mux_0, 1))
         self.connect((self.blocks_repack_bits_bb_0_0_0_0_0_0, 0), (self.digital_protocol_formatter_bb_0, 0))
         self.connect((self.blocks_stream_mux_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
-        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.blocks_repack_bits_bb_0_0_0_0, 0))
+        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_crc32_bb_0, 0))
         self.connect((self.blocks_tagged_stream_mux_0, 0), (self.digital_constellation_modulator_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_mux_0, 1))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_stream_mux_0, 0))
-        self.connect((self.channels_channel_model_0, 0), (self.digital_pfb_clock_sync_xxx_0_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.digital_symbol_sync_xx_0_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.channels_channel_model_0, 0))
         self.connect((self.digital_constellation_soft_decoder_cf_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
         self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.fec_extended_tagged_decoder_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_constellation_soft_decoder_cf_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_1, 0))
-        self.connect((self.digital_pfb_clock_sync_xxx_0_0, 0), (self.digital_costas_loop_cc_0, 0))
+        self.connect((self.digital_crc32_bb_0, 0), (self.blocks_repack_bits_bb_0_0_0_0, 0))
+        self.connect((self.digital_crc32_bb_0_0, 0), (self.blocks_keep_m_in_n_0, 0))
+        self.connect((self.digital_crc32_bb_0_0, 0), (self.blocks_keep_m_in_n_0_0, 0))
         self.connect((self.digital_protocol_formatter_bb_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+        self.connect((self.digital_symbol_sync_xx_0_0, 0), (self.digital_costas_loop_cc_0, 0))
         self.connect((self.fec_extended_tagged_decoder_0, 0), (self.blocks_char_to_float_1_1_0, 0))
         self.connect((self.fec_extended_tagged_decoder_0, 0), (self.blocks_repack_bits_bb_0_0_0, 0))
         self.connect((self.fec_extended_tagged_encoder_0, 0), (self.blocks_repack_bits_bb_0_0_0_0_0_0, 0))
@@ -281,7 +288,8 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_packetCounterLength(self, packetCounterLength):
         self.packetCounterLength = packetCounterLength
-        self.set_payloadLength((self.infoLength+self.packetCounterLength))
+        self.set_packetLength((self.infoLength+self.packetCounterLength+4))
+        self.set_dataLength((self.infoLength+self.packetCounterLength))
         self.blocks_keep_m_in_n_0_0.set_offset(self.packetCounterLength)
         self.blocks_keep_m_in_n_0.set_m(self.packetCounterLength)
 
@@ -290,19 +298,16 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_infoLength(self, infoLength):
         self.infoLength = infoLength
-        self.set_payloadLength((self.infoLength+self.packetCounterLength))
+        self.set_packetLength((self.infoLength+self.packetCounterLength+4))
+        self.set_dataLength((self.infoLength+self.packetCounterLength))
         self.blocks_keep_m_in_n_0_0.set_m(self.infoLength)
 
-    def get_payloadLength(self):
-        return self.payloadLength
+    def get_packetLength(self):
+        return self.packetLength
 
-    def set_payloadLength(self, payloadLength):
-        self.payloadLength = payloadLength
-        self.set_frameLength(self.payloadLength+12)
-        self.blocks_stream_to_tagged_stream_0.set_packet_len(self.payloadLength)
-        self.blocks_stream_to_tagged_stream_0.set_packet_len_pmt(self.payloadLength)
-        self.blocks_keep_m_in_n_0_0.set_n(self.payloadLength)
-        self.blocks_keep_m_in_n_0.set_n(self.payloadLength)
+    def set_packetLength(self, packetLength):
+        self.packetLength = packetLength
+        self.set_frameLength(self.packetLength+12)
 
     def get_sps(self):
         return self.sps
@@ -317,6 +322,7 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_nfilts(self, nfilts):
         self.nfilts = nfilts
         self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts*self.sps, 1.0, self.excess_bw, 45*self.nfilts))
+        self.digital_symbol_sync_xx_0_0.set_damping_factor(2*self.nfilts)
 
     def get_frameLength(self):
         return self.frameLength
@@ -336,7 +342,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_snr(self, snr):
         self.snr = snr
-        self.channels_channel_model_0.set_noise_voltage(self.snr)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -351,13 +356,22 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_rrc_taps(self, rrc_taps):
         self.rrc_taps = rrc_taps
-        self.digital_pfb_clock_sync_xxx_0_0.update_taps((self.rrc_taps))
 
     def get_order_costas(self):
         return self.order_costas
 
     def set_order_costas(self, order_costas):
         self.order_costas = order_costas
+
+    def get_dataLength(self):
+        return self.dataLength
+
+    def set_dataLength(self, dataLength):
+        self.dataLength = dataLength
+        self.blocks_stream_to_tagged_stream_0.set_packet_len(self.dataLength)
+        self.blocks_stream_to_tagged_stream_0.set_packet_len_pmt(self.dataLength)
+        self.blocks_keep_m_in_n_0_0.set_n(self.dataLength)
+        self.blocks_keep_m_in_n_0.set_n(self.dataLength)
 
     def get_MO(self):
         return self.MO
