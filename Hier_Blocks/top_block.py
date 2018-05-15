4b27ec10a59aa55b908350954931e9ee0b96fa1d
bunnmv <bunnmv@gmail.com>
2018-05-15 14:18:59 -0300

    Flowgraph with selectors working

--- Hier_Blocks/top_block.py
+++ Hier_Blocks/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Tue May 15 11:55:30 2018
+# Generated: Tue May 15 14:16:56 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -34,10 +34,12 @@ from rx_inner_8psk import rx_inner_8psk  # grc-generated hier_block
 from rx_inner_bpsk import rx_inner_bpsk  # grc-generated hier_block
 from rx_inner_qpsk import rx_inner_qpsk  # grc-generated hier_block
 from rx_outer_convolutional import rx_outer_convolutional  # grc-generated hier_block
+from rx_outer_dummy import rx_outer_dummy  # grc-generated hier_block
 from tx_inner_8psk import tx_inner_8psk  # grc-generated hier_block
 from tx_inner_bpsk import tx_inner_bpsk  # grc-generated hier_block
 from tx_inner_qpsk import tx_inner_qpsk  # grc-generated hier_block
 from tx_outer_CE import tx_outer_CE  # grc-generated hier_block
+from tx_outer_dummy import tx_outer_dummy  # grc-generated hier_block
 import RWN
 import numpy as np
 import pmt
@@ -83,7 +85,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 32000
         self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts*sps, 1.0, excess_bw, 45*nfilts)
         self.packetLength = packetLength = (infoLength+packetCounterLength+4)
-        self.mod_select = mod_select = 0
+        self.mod_select = mod_select = 2
         self.len_tag_name_rx = len_tag_name_rx = "len_key2"
         self.len_tag_name = len_tag_name = "len_key"
         self.excess_bw_0 = excess_bw_0 = 0.35
@@ -136,6 +138,13 @@ class top_block(gr.top_block, Qt.QWidget):
         self._FEC_button_group.buttonClicked[int].connect(
         	lambda i: self.set_FEC(self._FEC_options[i]))
         self.top_layout.addWidget(self._FEC_group_box)
+        self.tx_outer_dummy_0 = tx_outer_dummy(
+            dataLength=dataLength,
+            infoLength=infoLength,
+            len_tag_name=len_tag_name,
+            packetCounterLength=packetCounterLength,
+            packetLength=packetLength,
+        )
         self.tx_outer_CE_0 = tx_outer_CE(
             dataLength=dataLength,
             infoLength=infoLength,
@@ -163,6 +172,12 @@ class top_block(gr.top_block, Qt.QWidget):
             len_tag_name=len_tag_name,
             payloadLength=packetLength +(packetLength*FEC),
             sps=sps,
+        )
+        self.rx_outer_dummy_0 = rx_outer_dummy(
+            access_code=access_code,
+            len_tag_name_rx=len_tag_name_rx,
+            packetCounterLength=packetCounterLength,
+            packetLength=packetLength,
         )
         self.rx_outer_convolutional_0 = rx_outer_convolutional(
             access_code=access_code,
@@ -194,7 +209,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
         	1024, #size
         	1, #samp_rate
-        	"", #name
+        	"FEC Decoded", #name
         	1 #number of inputs
         )
         self.qtgui_time_sink_x_1.set_update_time(0.10)
@@ -239,6 +254,47 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
+        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
+        	1024, #size
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_const_sink_x_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(False)
+        self.qtgui_const_sink_x_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_const_sink_x_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "red", "red", "red",
+                  "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
         self.channels_channel_model_0 = channels.channel_model(
         	noise_voltage=0.2,
         	frequency_offset=0.0,
@@ -247,17 +303,27 @@ class top_block(gr.top_block, Qt.QWidget):
         	noise_seed=0,
         	block_tags=True
         )
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate*100,True)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate*1000,True)
+        self.blocks_null_source_1_0 = blocks.null_source(gr.sizeof_char*1)
+        self.blocks_null_source_1 = blocks.null_source(gr.sizeof_char*1)
+        self.blocks_null_source_0 = blocks.null_source(gr.sizeof_char*1)
+        self.blocks_null_sink_1 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_char*1)
         self.blocks_keep_m_in_n_0_0 = blocks.keep_m_in_n(gr.sizeof_char, infoLength, dataLength, packetCounterLength)
-        self.blocks_head_0 = blocks.head(gr.sizeof_char*1, 110000)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/Users/marcusbunn/Documents/engtelecom/TCC/programming/SDR/2600-0.txt', True)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/marcusbunn/Desktop/payload.txt', False)
         self.blocks_file_sink_0_0.set_unbuffered(True)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.RWN_selector_3_1_ff_0 = RWN.selector_3_1_ff(mod_select, False)
+        self.RWN_selector_3_1_cc_1 = RWN.selector_3_1_cc(mod_select, False)
         self.RWN_selector_3_1_cc_0 = RWN.selector_3_1_cc(mod_select, False)
+        self.RWN_selector_3_1_bb_1_0 = RWN.selector_3_1_bb(FEC, False)
+        self.RWN_selector_3_1_bb_1 = RWN.selector_3_1_bb(FEC, False)
+        self.RWN_selector_3_1_bb_0 = RWN.selector_3_1_bb(FEC, True)
+        self.RWN_selector_1_3_ff_0 = RWN.selector_1_3_ff(FEC, False)
         self.RWN_selector_1_3_cc_0 = RWN.selector_1_3_cc(mod_select, False)
+        self.RWN_selector_1_3_bb_1 = RWN.selector_1_3_bb(FEC, False)
         self.RWN_selector_1_3_bb_0 = RWN.selector_1_3_bb(mod_select, False)
 
 
@@ -268,26 +334,44 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.RWN_selector_1_3_bb_0, 0), (self.tx_inner_8psk_0, 0))
         self.connect((self.RWN_selector_1_3_bb_0, 2), (self.tx_inner_bpsk_0, 0))
         self.connect((self.RWN_selector_1_3_bb_0, 1), (self.tx_inner_qpsk_0, 0))
+        self.connect((self.RWN_selector_1_3_bb_1, 2), (self.blocks_null_sink_0, 0))
+        self.connect((self.RWN_selector_1_3_bb_1, 1), (self.tx_outer_CE_0, 0))
+        self.connect((self.RWN_selector_1_3_bb_1, 0), (self.tx_outer_dummy_0, 0))
         self.connect((self.RWN_selector_1_3_cc_0, 0), (self.rx_inner_8psk_0, 0))
         self.connect((self.RWN_selector_1_3_cc_0, 2), (self.rx_inner_bpsk_0, 0))
         self.connect((self.RWN_selector_1_3_cc_0, 1), (self.rx_inner_qpsk_0, 0))
+        self.connect((self.RWN_selector_1_3_ff_0, 2), (self.blocks_null_sink_1, 0))
+        self.connect((self.RWN_selector_1_3_ff_0, 1), (self.rx_outer_convolutional_0, 0))
+        self.connect((self.RWN_selector_1_3_ff_0, 0), (self.rx_outer_dummy_0, 0))
+        self.connect((self.RWN_selector_3_1_bb_0, 0), (self.RWN_selector_1_3_bb_0, 0))
+        self.connect((self.RWN_selector_3_1_bb_1, 0), (self.blocks_keep_m_in_n_0_0, 0))
+        self.connect((self.RWN_selector_3_1_bb_1_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.RWN_selector_3_1_cc_0, 0), (self.channels_channel_model_0, 0))
-        self.connect((self.RWN_selector_3_1_ff_0, 0), (self.rx_outer_convolutional_0, 0))
+        self.connect((self.RWN_selector_3_1_cc_1, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.RWN_selector_3_1_ff_0, 0), (self.RWN_selector_1_3_ff_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_1, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_head_0, 0))
-        self.connect((self.blocks_head_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.blocks_keep_m_in_n_0_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.tx_outer_CE_0, 0))
+        self.connect((self.blocks_null_source_0, 0), (self.RWN_selector_3_1_bb_0, 2))
+        self.connect((self.blocks_null_source_1, 0), (self.RWN_selector_3_1_bb_1_0, 2))
+        self.connect((self.blocks_null_source_1_0, 0), (self.RWN_selector_3_1_bb_1, 2))
+        self.connect((self.blocks_throttle_0, 0), (self.RWN_selector_1_3_bb_1, 0))
         self.connect((self.channels_channel_model_0, 0), (self.RWN_selector_1_3_cc_0, 0))
-        self.connect((self.rx_inner_8psk_0, 0), (self.RWN_selector_3_1_ff_0, 0))
-        self.connect((self.rx_inner_bpsk_0, 0), (self.RWN_selector_3_1_ff_0, 2))
-        self.connect((self.rx_inner_qpsk_0, 0), (self.RWN_selector_3_1_ff_0, 1))
-        self.connect((self.rx_outer_convolutional_0, 1), (self.blocks_char_to_float_0, 0))
-        self.connect((self.rx_outer_convolutional_0, 0), (self.blocks_keep_m_in_n_0_0, 0))
+        self.connect((self.rx_inner_8psk_0, 0), (self.RWN_selector_3_1_cc_1, 0))
+        self.connect((self.rx_inner_8psk_0, 1), (self.RWN_selector_3_1_ff_0, 0))
+        self.connect((self.rx_inner_bpsk_0, 0), (self.RWN_selector_3_1_cc_1, 2))
+        self.connect((self.rx_inner_bpsk_0, 1), (self.RWN_selector_3_1_ff_0, 2))
+        self.connect((self.rx_inner_qpsk_0, 0), (self.RWN_selector_3_1_cc_1, 1))
+        self.connect((self.rx_inner_qpsk_0, 1), (self.RWN_selector_3_1_ff_0, 1))
+        self.connect((self.rx_outer_convolutional_0, 0), (self.RWN_selector_3_1_bb_1, 1))
+        self.connect((self.rx_outer_convolutional_0, 1), (self.RWN_selector_3_1_bb_1_0, 1))
+        self.connect((self.rx_outer_dummy_0, 0), (self.RWN_selector_3_1_bb_1, 0))
+        self.connect((self.rx_outer_dummy_0, 1), (self.RWN_selector_3_1_bb_1_0, 0))
         self.connect((self.tx_inner_8psk_0, 0), (self.RWN_selector_3_1_cc_0, 0))
         self.connect((self.tx_inner_bpsk_0, 0), (self.RWN_selector_3_1_cc_0, 2))
         self.connect((self.tx_inner_qpsk_0, 0), (self.RWN_selector_3_1_cc_0, 1))
-        self.connect((self.tx_outer_CE_0, 0), (self.RWN_selector_1_3_bb_0, 0))
+        self.connect((self.tx_outer_CE_0, 0), (self.RWN_selector_3_1_bb_0, 1))
+        self.connect((self.tx_outer_dummy_0, 0), (self.RWN_selector_3_1_bb_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -314,7 +398,9 @@ class top_block(gr.top_block, Qt.QWidget):
         self.packetCounterLength = packetCounterLength
         self.set_packetLength((self.infoLength+self.packetCounterLength+4))
         self.set_dataLength((self.infoLength+self.packetCounterLength))
+        self.tx_outer_dummy_0.set_packetCounterLength(self.packetCounterLength)
         self.tx_outer_CE_0.set_packetCounterLength(self.packetCounterLength)
+        self.rx_outer_dummy_0.set_packetCounterLength(self.packetCounterLength)
         self.rx_outer_convolutional_0.set_packetCounterLength(self.packetCounterLength)
         self.blocks_keep_m_in_n_0_0.set_offset(self.packetCounterLength)
 
@@ -335,6 +421,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.infoLength = infoLength
         self.set_packetLength((self.infoLength+self.packetCounterLength+4))
         self.set_dataLength((self.infoLength+self.packetCounterLength))
+        self.tx_outer_dummy_0.set_infoLength(self.infoLength)
         self.tx_outer_CE_0.set_infoLength(self.infoLength)
         self.blocks_keep_m_in_n_0_0.set_m(self.infoLength)
 
@@ -356,7 +443,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate*100)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate*1000)
 
     def get_rrc_taps(self):
         return self.rrc_taps
@@ -372,10 +459,12 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_packetLength(self, packetLength):
         self.packetLength = packetLength
+        self.tx_outer_dummy_0.set_packetLength(self.packetLength)
         self.tx_outer_CE_0.set_packetLength(self.packetLength)
         self.tx_inner_qpsk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
         self.tx_inner_bpsk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
         self.tx_inner_8psk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
+        self.rx_outer_dummy_0.set_packetLength(self.packetLength)
         self.rx_outer_convolutional_0.set_packetLength(self.packetLength)
 
     def get_mod_select(self):
@@ -385,6 +474,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.mod_select = mod_select
         self._mod_select_callback(self.mod_select)
         self.RWN_selector_3_1_ff_0.set_selected(self.mod_select)
+        self.RWN_selector_3_1_cc_1.set_selected(self.mod_select)
         self.RWN_selector_3_1_cc_0.set_selected(self.mod_select)
         self.RWN_selector_1_3_cc_0.set_selected(self.mod_select)
         self.RWN_selector_1_3_bb_0.set_selected(self.mod_select)
@@ -394,6 +484,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_len_tag_name_rx(self, len_tag_name_rx):
         self.len_tag_name_rx = len_tag_name_rx
+        self.rx_outer_dummy_0.set_len_tag_name_rx(self.len_tag_name_rx)
         self.rx_outer_convolutional_0.set_len_tag_name_rx(self.len_tag_name_rx)
 
     def get_len_tag_name(self):
@@ -401,6 +492,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_len_tag_name(self, len_tag_name):
         self.len_tag_name = len_tag_name
+        self.tx_outer_dummy_0.set_len_tag_name(self.len_tag_name)
         self.tx_outer_CE_0.set_len_tag_name(self.len_tag_name)
         self.tx_inner_qpsk_0.set_len_tag_name(self.len_tag_name)
         self.tx_inner_bpsk_0.set_len_tag_name(self.len_tag_name)
@@ -417,6 +509,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_dataLength(self, dataLength):
         self.dataLength = dataLength
+        self.tx_outer_dummy_0.set_dataLength(self.dataLength)
         self.tx_outer_CE_0.set_dataLength(self.dataLength)
         self.blocks_keep_m_in_n_0_0.set_n(self.dataLength)
 
@@ -428,6 +521,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.tx_inner_qpsk_0.set_access_code(self.access_code)
         self.tx_inner_bpsk_0.set_access_code(self.access_code)
         self.tx_inner_8psk_0.set_access_code(self.access_code)
+        self.rx_outer_dummy_0.set_access_code(self.access_code)
         self.rx_outer_convolutional_0.set_access_code(self.access_code)
 
     def get_FEC(self):
@@ -439,6 +533,11 @@ class top_block(gr.top_block, Qt.QWidget):
         self.tx_inner_qpsk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
         self.tx_inner_bpsk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
         self.tx_inner_8psk_0.set_payloadLength(self.packetLength +(self.packetLength*self.FEC))
+        self.RWN_selector_3_1_bb_1_0.set_selected(self.FEC)
+        self.RWN_selector_3_1_bb_1.set_selected(self.FEC)
+        self.RWN_selector_3_1_bb_0.set_selected(self.FEC)
+        self.RWN_selector_1_3_ff_0.set_selected(self.FEC)
+        self.RWN_selector_1_3_bb_1.set_selected(self.FEC)
 
 
 def main(top_block_cls=top_block, options=None):
